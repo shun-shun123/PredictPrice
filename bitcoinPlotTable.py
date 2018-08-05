97b925d47c11b402aa18674d6b27747cdfb0e966
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-05 22:06:19 +0900

    bitcoinの情報を表示

---
+++ bitcoinPlotTable.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+
+# コインマーケットキャップからデータをスクレイピング
+bitcoin_market_info = pd.read_html("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
+print(bitcoin_market_info)
+# Dateを文字列から日付フォーマットへ変換
+bitcoin_market_info = bitcoin_market_info.assign(Date=pd.to_datetime(bitcoin_market_info['Date']))
+# 取引高が'-'の欠損データを0へ変換
+bitcoin_market_info.loc[bitcoin_market_info['Volume']=='-', 'Volume'] = 0
+# intへ変換
+bitcoin_market_info['Volume'] = bitcoin_market_info['Volume'].astype('int64')
+# データセットのヘッド情報の確認
+print("\n*****************データセットのヘッダ情報****************\n")
+print(bitcoin_market_info.head())
