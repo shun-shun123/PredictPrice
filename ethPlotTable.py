7f16b3b2ab5e406e07ca47cc6ca34758bc4d2f04
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-05 22:22:43 +0900

    情報取得をモジュール化

--- ethPlotTable.py
+++ ethPlotTable.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 import datetime
 import numpy as np
 
-# イーサリアムの価格データを所定のURLからスクレイピング
-eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
-# データの処理とヘッド情報の表示
-eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
+def getEthMarketInfo():
+    # イーサリアムの価格データを所定のURLからスクレイピング
+    eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/historical-data/?start=20130428&end="+time.strftime("%Y%m%d"))[0]
+    # データの処理とヘッド情報の表示
+    eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
+    return eth_market_info
+
 print("\n*****************データセットのヘッダ情報****************\n")
-print(eth_market_info.head())
+print(getEthMarketInfo().head())
