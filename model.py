461bcf918897148d8f4623f48e3d0f5b864c6814
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-06 23:54:55 +0900

    modelの損失関数をmean_squared_errorに変更

--- model.py
+++ model.py
@@ -1,11 +1,10 @@
-from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Activation, Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
 # モデル構築
-def createModel(inputs, output_size, neurons, active_func='linear', dropout=0.25, loss='mae', optimizer='adam'):
+def createModel(inputs, output_size, neurons, active_func='linear', dropout=0.25, loss='mean_squared_error', optimizer='adam'):
     model = Sequential()
 
     model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
