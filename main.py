cff0904b1652efa7eea4e9532a39ee53cfb8925c
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-08-08 20:43:50 +0900

    予測結果をプロットしてみる

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import dataCreate
 import numpy as np
 import model
 import matplotlib.pyplot as plt
+import datetime
 
 market_info = dataCreate.createDataSet()
 
@@ -76,4 +77,56 @@ ax1.plot(eth_history.epoch, eth_history.history['loss'])
 ax1.set_title("TrainingError")
 ax1.set_ylabel('Mean Absolute Error(MAE)', fontsize=12)
 ax1.set_xlabel("# Epochs", fontsize=12)
+plt.show()
+
+
+# プロッティングと予測の処理を一緒に行う
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+fig, ax1 = plt.subplots(1, 1)
+ax1.set_xticks([datetime.date(i, j, 1) for i in range(2013, 2019) for j in [1, 5, 9]])
+ax1.set_xticklabels([datetime.date(i, j, 1).strftime('%d %y') for i in range(2013, 2019) for j in [1, 5, 9]])
+ax1.plot(model_data[model_data['Date'] < split_date]['Date'][window_len:].astype(datetime.datetime),
+         training_set['Close**_y'][window_len:], label='Actual')
+ax1.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype(datetime.datetime),
+         ((np.transpose(eth_model.predict(LSTM_training_inputs))+1) * training_set['Close**_y'].values[:-window_len])[0],
+         label='Predicted')
+ax1.set_title('Training Set: Single Timepoint Prediction')
+ax1.set_ylabel('Ethereum Price ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.15, 1), loc=2, borderaxespad=0., prop={'size': 14})
+ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(eth_model.predict(LSTM_training_inputs))+1)-\
+                                        (training_set['Close**_y'].values[window_len:])/(training_set['Close**_y'].values[:-window_len]))),
+             xy=(0.75, 0.9),  xycoords='axes fraction',
+             xytext=(0.75, 0.9), textcoords='axes fraction')
+# 下記コードはこちらのURL参照しました：http://akuederle.com/matplotlib-zoomed-up-inset
+axins = zoomed_inset_axes(ax1, 3.35, loc=10)
+axins.set_xticks([datetime.date(i,j,1) for i in range(2013,2019) for j in [1, 5, 9]])
+axins.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype(datetime.datetime),
+         training_set['Close**_y'][window_len:], label='Actual')
+axins.plot(model_data[model_data['Date']< split_date]['Date'][window_len:].astype(datetime.datetime),
+         ((np.transpose(eth_model.predict(LSTM_training_inputs))+1) * training_set['Close**_y'].values[:-window_len])[0],
+         label='Predicted')
+axins.set_xlim([datetime.date(2017, 3, 1), datetime.date(2017, 5, 1)])
+axins.set_ylim([10, 60])
+axins.set_xticklabels('')
+mark_inset(ax1, axins, loc1=1, loc2=3, fc="none", ec="0.5")
+plt.show()
+
+# LSTMモデルがみたことの無い時期の予測
+fig, ax1 = plt.subplots(1, 1)
+ax1.set_xticks([datetime.date(2017, i + 1, 1) for i in range(12)])
+ax1.set_xticklabels([datetime.date(2017,i+1,1).strftime('%b %d %Y') for i in range(12)])
+ax1.plot(model_data[model_data['Date'] >= split_date]['Date'][window_len:].astype(datetime.datetime),
+         test_set['Close**_y'][window_len:], label='Actual')
+ax1.plot(model_data[model_data['Date'] >= split_date]['Date'][window_len:].astype(datetime.datetime),
+         ((np.transpose(eth_model.predict(LSTM_test_inputs))+1) * test_set['Close**_y'].values[:-window_len])[0],
+         label='Predicted')
+ax1.annotate('MAE: %.4f'%np.mean(np.abs((np.transpose(eth_model.predict(LSTM_test_inputs))+1)-\
+            (test_set['Close**_y'].values[window_len:])/(test_set['Close**_y'].values[:-window_len]))),
+             xy=(0.75, 0.9),  xycoords='axes fraction',
+            xytext=(0.75, 0.9), textcoords='axes fraction')
+ax1.set_title('Test Set: Single Timepoint Prediction', fontsize=13)
+ax1.set_ylabel('Ethereum Price ($)',fontsize=12)
+ax1.legend(bbox_to_anchor=(0.1, 1), loc=2, borderaxespad=0., prop={'size': 14})
 plt.show()
